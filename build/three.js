e0443073a3aa8ddbdec49f6f86a3aadee50bfee9
3jsLive <54175649+3jsLive@users.noreply.github.com>
2019-01-15 17:39:59 -0800

    db47ecba43fef53964773da6229786925502975a

--- build/three.js
+++ build/three.js
@@ -40262,7 +40262,7 @@
 			var source = this.context.createBufferSource();
 
 			source.buffer = this.buffer;
-			this.setDetune( this.detune );
+			source.detune.value = this.detune;
 			source.loop = this.loop;
 			source.onended = this.onEnded.bind( this );
 			source.playbackRate.setValueAtTime( this.playbackRate, this.startTime );
@@ -40394,13 +40394,6 @@
 		setDetune: function ( value ) {
 
 			this.detune = value;
-
-			if ( this.source.detune === undefined ) {
-
-				console.warn( 'THREE.Audio: AudioBufferSourceNode.detune not supported by the browser.' );
-				return;
-
-			}
 
 			if ( this.isPlaying === true ) {
 
