679e6519b4f6a1e95f2658a07d3b6464e66c00c7
3jsLive <54175649+3jsLive@users.noreply.github.com>
2019-01-20 14:58:03 +0800

    eb572f973adbe852d2cc9d289f4204af42d52460

--- build/three.js
+++ build/three.js
@@ -14488,7 +14488,6 @@
 									if ( intersection ) {
 
 										intersection.faceIndex = Math.floor( j / 3 ); // triangle number in indexed buffer semantics
-										intersection.face.materialIndex = group.materialIndex;
 										intersects.push( intersection );
 
 									}
@@ -14546,7 +14545,6 @@
 									if ( intersection ) {
 
 										intersection.faceIndex = Math.floor( j / 3 ); // triangle number in non-indexed buffer semantics
-										intersection.face.materialIndex = group.materialIndex;
 										intersects.push( intersection );
 
 									}
