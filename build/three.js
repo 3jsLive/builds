412b74bd7e1207b9f50eaa4887336cb662a2a992
3jsLive <54175649+3jsLive@users.noreply.github.com>
2019-02-01 13:18:48 +0100

    7d75fe61be577013bda36483c5d041be1ce9343e

--- build/three.js
+++ build/three.js
@@ -13065,17 +13065,8 @@
 		this.defines = {};
 		this.uniforms = {};
 
-		this.vertexShader = /* glsl */ `
-	void main() {
-		gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-	}
-	`;
-
-		this.fragmentShader = /* glsl */ `
-	void main() {
-		gl_FragColor = vec4( 1.0, 0.0, 0.0, 1.0 );
-	}
-	`;
+		this.vertexShader = 'void main() {\n\tgl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );\n}';
+		this.fragmentShader = 'void main() {\n\tgl_FragColor = vec4( 1.0, 0.0, 0.0, 1.0 );\n}';
 
 		this.linewidth = 1;
 
@@ -20166,7 +20157,7 @@
 
 			}
 
-			if ( internalFormat === 33325 || internalFormat === 33326 ||
+			if ( internalFormat === 33325 || internalFormat === 33326 ||
 				internalFormat === 34842 || internalFormat === 34836 ) {
 
 				extensions.get( 'EXT_color_buffer_float' );
