ddc15e2a96e124d0a09b2d9d798520261bfca6b2
3jsLive <54175649+3jsLive@users.noreply.github.com>
2019-02-13 12:38:10 -0700

    e3fbcd7e592c17e78a9727f762cc1c4f759af252

--- build/three.js
+++ build/three.js
@@ -185,7 +185,7 @@
 
 	} );
 
-	var REVISION = '102dev';
+	var REVISION = '101';
 	var MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2 };
 	var CullFaceNone = 0;
 	var CullFaceBack = 1;
@@ -6161,7 +6161,7 @@
 
 	var project_vertex = "vec4 mvPosition = modelViewMatrix * vec4( transformed, 1.0 );\ngl_Position = projectionMatrix * mvPosition;";
 
-	var dithering_fragment = "#if defined( DITHERING )\n\tgl_FragColor.rgb = dithering( gl_FragColor.rgb );\n#endif";
+	var dithering_fragment = "#if defined( DITHERING )\n  gl_FragColor.rgb = dithering( gl_FragColor.rgb );\n#endif";
 
 	var dithering_pars_fragment = "#if defined( DITHERING )\n\tvec3 dithering( vec3 color ) {\n\t\tfloat grid_position = rand( gl_FragCoord.xy );\n\t\tvec3 dither_shift_RGB = vec3( 0.25 / 255.0, -0.25 / 255.0, 0.25 / 255.0 );\n\t\tdither_shift_RGB = mix( 2.0 * dither_shift_RGB, -2.0 * dither_shift_RGB, grid_position );\n\t\treturn color + dither_shift_RGB;\n\t}\n#endif";
 
@@ -6189,7 +6189,7 @@
 
 	var specularmap_pars_fragment = "#ifdef USE_SPECULARMAP\n\tuniform sampler2D specularMap;\n#endif";
 
-	var tonemapping_fragment = "#if defined( TONE_MAPPING )\n\tgl_FragColor.rgb = toneMapping( gl_FragColor.rgb );\n#endif";
+	var tonemapping_fragment = "#if defined( TONE_MAPPING )\n  gl_FragColor.rgb = toneMapping( gl_FragColor.rgb );\n#endif";
 
 	var tonemapping_pars_fragment = "#ifndef saturate\n\t#define saturate(a) clamp( a, 0.0, 1.0 )\n#endif\nuniform float toneMappingExposure;\nuniform float toneMappingWhitePoint;\nvec3 LinearToneMapping( vec3 color ) {\n\treturn toneMappingExposure * color;\n}\nvec3 ReinhardToneMapping( vec3 color ) {\n\tcolor *= toneMappingExposure;\n\treturn saturate( color / ( vec3( 1.0 ) + color ) );\n}\n#define Uncharted2Helper( x ) max( ( ( x * ( 0.15 * x + 0.10 * 0.50 ) + 0.20 * 0.02 ) / ( x * ( 0.15 * x + 0.50 ) + 0.20 * 0.30 ) ) - 0.02 / 0.30, vec3( 0.0 ) )\nvec3 Uncharted2ToneMapping( vec3 color ) {\n\tcolor *= toneMappingExposure;\n\treturn saturate( Uncharted2Helper( color ) / Uncharted2Helper( vec3( toneMappingWhitePoint ) ) );\n}\nvec3 OptimizedCineonToneMapping( vec3 color ) {\n\tcolor *= toneMappingExposure;\n\tcolor = max( vec3( 0.0 ), color - 0.004 );\n\treturn pow( ( color * ( 6.2 * color + 0.5 ) ) / ( color * ( 6.2 * color + 1.7 ) + 0.06 ), vec3( 2.2 ) );\n}\nvec3 ACESFilmicToneMapping( vec3 color ) {\n\tcolor *= toneMappingExposure;\n\treturn saturate( ( color * ( 2.51 * color + 0.03 ) ) / ( color * ( 2.43 * color + 0.59 ) + 0.14 ) );\n}";
 
@@ -10048,7 +10048,20 @@
 
 				for ( var key in parameters ) {
 
-					if ( parameters[ key ] !== undefined ) data[ key ] = parameters[ key ];
+					var value = parameters[ key ];
+					if ( value !== undefined ) {
+
+						if ( value.toJSON ) {
+
+							data[ key ] = value.toJSON();
+
+						} else {
+
+							data[ key ] = value;
+
+						}
+
+					}
 
 				}
 
@@ -12142,7 +12155,20 @@
 
 				for ( var key in parameters ) {
 
-					if ( parameters[ key ] !== undefined ) data[ key ] = parameters[ key ];
+					var value = parameters[ key ];
+					if ( value !== undefined ) {
+
+						if ( value.toJSON ) {
+
+							data[ key ] = value.toJSON();
+
+						} else {
+
+							data[ key ] = value;
+
+						}
+
+					}
 
 				}
 
@@ -13035,10 +13061,6 @@
 
 	} );
 
-	var default_vertex = "void main() {\n\tgl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );\n}";
-
-	var default_fragment = "void main() {\n\tgl_FragColor = vec4( 1.0, 0.0, 0.0, 1.0 );\n}";
-
 	/**
 	 * @author alteredq / http://alteredqualia.com/
 	 *
@@ -13069,8 +13091,8 @@
 		this.defines = {};
 		this.uniforms = {};
 
-		this.vertexShader = default_vertex;
-		this.fragmentShader = default_fragment;
+		this.vertexShader = 'void main() {\n\tgl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );\n}';
+		this.fragmentShader = 'void main() {\n\tgl_FragColor = vec4( 1.0, 0.0, 0.0, 1.0 );\n}';
 
 		this.linewidth = 1;
 
@@ -15768,7 +15790,6 @@
 
 		images = images !== undefined ? images : [];
 		mapping = mapping !== undefined ? mapping : CubeReflectionMapping;
-		format = format !== undefined ? format : RGBFormat;
 
 		Texture.call( this, images, mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy, encoding );
 
@@ -15817,8 +15838,6 @@
 
 		this.magFilter = NearestFilter;
 		this.minFilter = NearestFilter;
-
-		this.wrapR = ClampToEdgeWrapping;
 
 		this.generateMipmaps = false;
 		this.flipY = false;
@@ -17874,7 +17893,7 @@
 
 			return a.renderOrder - b.renderOrder;
 
-		} else if ( a.z !== b.z ) {
+		} if ( a.z !== b.z ) {
 
 			return b.z - a.z;
 
@@ -20164,7 +20183,7 @@
 
 			}
 
-			if ( internalFormat === 33325 || internalFormat === 33326 ||
+			if ( internalFormat === 33325 || internalFormat === 33326 ||
 				internalFormat === 34842 || internalFormat === 34836 ) {
 
 				extensions.get( 'EXT_color_buffer_float' );
@@ -20231,10 +20250,23 @@
 
 			var textureProperties = properties.get( texture );
 
-			if ( textureProperties.__webglInit === undefined ) return;
+			if ( texture.image && textureProperties.__image__webglTextureCube ) {
 
-			_gl.deleteTexture( textureProperties.__webglTexture );
+				// cube texture
 
+				_gl.deleteTexture( textureProperties.__image__webglTextureCube );
+
+			} else {
+
+				// 2D texture
+
+				if ( textureProperties.__webglInit === undefined ) return;
+
+				_gl.deleteTexture( textureProperties.__webglTexture );
+
+			}
+
+			// remove all webgl properties
 			properties.remove( texture );
 
 		}
@@ -20331,6 +20363,7 @@
 
 		}
 
+
 		function setTextureCube( texture, slot ) {
 
 			var textureProperties = properties.get( texture );
@@ -20339,10 +20372,18 @@
 
 				if ( texture.version > 0 && textureProperties.__version !== texture.version ) {
 
-					initTexture( textureProperties, texture );
+					if ( ! textureProperties.__image__webglTextureCube ) {
+
+						texture.addEventListener( 'dispose', onTextureDispose );
+
+						textureProperties.__image__webglTextureCube = _gl.createTexture();
+
+						info.memory.textures ++;
+
+					}
 
 					state.activeTexture( 33984 + slot );
-					state.bindTexture( 34067, textureProperties.__webglTexture );
+					state.bindTexture( 34067, textureProperties.__image__webglTextureCube );
 
 					_gl.pixelStorei( 37440, texture.flipY );
 
@@ -20443,7 +20484,7 @@
 				} else {
 
 					state.activeTexture( 33984 + slot );
-					state.bindTexture( 34067, textureProperties.__webglTexture );
+					state.bindTexture( 34067, textureProperties.__image__webglTextureCube );
 
 				}
 
@@ -20467,12 +20508,6 @@
 				_gl.texParameteri( textureType, 10242, utils.convert( texture.wrapS ) );
 				_gl.texParameteri( textureType, 10243, utils.convert( texture.wrapT ) );
 
-				if ( textureType === 32879 ) {
-
-					_gl.texParameteri( textureType, 32882, utils.convert( texture.wrapR ) );
-
-				}
-
 				_gl.texParameteri( textureType, 10240, utils.convert( texture.magFilter ) );
 				_gl.texParameteri( textureType, 10241, utils.convert( texture.minFilter ) );
 
@@ -20480,12 +20515,6 @@
 
 				_gl.texParameteri( textureType, 10242, 33071 );
 				_gl.texParameteri( textureType, 10243, 33071 );
-
-				if ( textureType === 32879 ) {
-
-					_gl.texParameteri( textureType, 32882, 33071 );
-
-				}
 
 				if ( texture.wrapS !== ClampToEdgeWrapping || texture.wrapT !== ClampToEdgeWrapping ) {
 
@@ -20522,7 +20551,20 @@
 
 		}
 
-		function initTexture( textureProperties, texture ) {
+		function uploadTexture( textureProperties, texture, slot ) {
+
+			var textureType;
+
+			if ( texture.isDataTexture3D ) {
+
+				textureType = 32879;
+
+			} else {
+
+				textureType = 3553;
+
+			}
+
 
 			if ( textureProperties.__webglInit === undefined ) {
 
@@ -20535,17 +20577,12 @@
 				info.memory.textures ++;
 
 			}
-
-		}
-
-		function uploadTexture( textureProperties, texture, slot ) {
-
-			var textureType = ( texture.isDataTexture3D ) ? 32879 : 3553;
-
-			initTexture( textureProperties, texture );
-
 			state.activeTexture( 33984 + slot );
+
+
 			state.bindTexture( textureType, textureProperties.__webglTexture );
+
+
 
 			_gl.pixelStorei( 37440, texture.flipY );
 			_gl.pixelStorei( 37441, texture.premultiplyAlpha );
@@ -22733,14 +22770,14 @@
 
 		this.setViewport = function ( x, y, width, height ) {
 
-			_viewport.set( x, y, width, height );
+			_viewport.set( x, _height - y - height, width, height );
 			state.viewport( _currentViewport.copy( _viewport ).multiplyScalar( _pixelRatio ) );
 
 		};
 
 		this.setScissor = function ( x, y, width, height ) {
 
-			_scissor.set( x, y, width, height );
+			_scissor.set( x, _height - y - height, width, height );
 			state.scissor( _currentScissor.copy( _scissor ).multiplyScalar( _pixelRatio ) );
 
 		};
@@ -23128,7 +23165,7 @@
 
 		function setupVertexAttributes( material, program, geometry ) {
 
-			if ( geometry && geometry.isInstancedBufferGeometry && ! capabilities.isWebGL2 ) {
+			if ( geometry && geometry.isInstancedBufferGeometry & ! capabilities.isWebGL2 ) {
 
 				if ( extensions.get( 'ANGLE_instanced_arrays' ) === null ) {
 
@@ -23331,23 +23368,7 @@
 
 		// Rendering
 
-		this.render = function ( scene, camera ) {
-
-			var renderTarget, forceClear;
-
-			if ( arguments[ 2 ] !== undefined ) {
-
-				console.warn( 'THREE.WebGLRenderer.render(): the renderTarget argument has been removed. Use .setRenderTarget() instead.' );
-				renderTarget = arguments[ 2 ];
-
-			}
-
-			if ( arguments[ 3 ] !== undefined ) {
-
-				console.warn( 'THREE.WebGLRenderer.render(): the forceClear argument has been removed. Use .clear() instead.' );
-				forceClear = arguments[ 3 ];
-
-			}
+		this.render = function ( scene, camera, renderTarget, forceClear ) {
 
 			if ( ! ( camera && camera.isCamera ) ) {
 
@@ -23420,11 +23441,13 @@
 
 			if ( this.info.autoReset ) this.info.reset();
 
-			if ( renderTarget !== undefined ) {
+			if ( renderTarget === undefined ) {
 
-				this.setRenderTarget( renderTarget );
+				renderTarget = null;
 
 			}
+
+			this.setRenderTarget( renderTarget );
 
 			//
 
@@ -23456,7 +23479,7 @@
 
 			//
 
-			if ( renderTarget !== undefined ) {
+			if ( renderTarget ) {
 
 				// Generate mipmap if we're using any kind of mipmap filtering
 
@@ -38171,6 +38194,18 @@
 
 							break;
 
+						case 'TubeGeometry':
+						case 'TubeBufferGeometry':
+
+							geometry = new Geometries[ data.type ](
+								new Curves[ data.path.type ]().fromJSON( data.path ),
+								data.tubularSegments,
+								data.radius,
+								data.radialSegments,
+								data.closed
+							);
+							break;
+
 						case 'LatheGeometry':
 						case 'LatheBufferGeometry':
 
@@ -39961,26 +39996,24 @@
 			renderTarget.texture.generateMipmaps = false;
 
 			renderTarget.activeCubeFace = 0;
-			renderer.setRenderTarget( renderTarget );
-
-			renderer.render( scene, cameraPX );
+			renderer.render( scene, cameraPX, renderTarget );
 
 			renderTarget.activeCubeFace = 1;
-			renderer.render( scene, cameraNX );
+			renderer.render( scene, cameraNX, renderTarget );
 
 			renderTarget.activeCubeFace = 2;
-			renderer.render( scene, cameraPY );
+			renderer.render( scene, cameraPY, renderTarget );
 
 			renderTarget.activeCubeFace = 3;
-			renderer.render( scene, cameraNY );
+			renderer.render( scene, cameraNY, renderTarget );
 
 			renderTarget.activeCubeFace = 4;
-			renderer.render( scene, cameraPZ );
+			renderer.render( scene, cameraPZ, renderTarget );
 
 			renderTarget.texture.generateMipmaps = generateMipmaps;
 
 			renderTarget.activeCubeFace = 5;
-			renderer.render( scene, cameraNZ );
+			renderer.render( scene, cameraNZ, renderTarget );
 
 			renderer.setRenderTarget( currentRenderTarget );
 
@@ -40662,7 +40695,7 @@
 
 				Object3D.prototype.updateMatrixWorld.call( this, force );
 
-				if ( this.hasPlaybackControl === true && this.isPlaying === false ) return;
+				if ( this.isPlaying === false ) return;
 
 				this.matrixWorld.decompose( position, quaternion, scale );
 
@@ -45009,106 +45042,6 @@
 	PolarGridHelper.prototype.constructor = PolarGridHelper;
 
 	/**
-	 * @author Mugen87 / http://github.com/Mugen87
-	 */
-
-	function PositionalAudioHelper( audio, range, divisionsInnerAngle, divisionsOuterAngle ) {
-
-		this.audio = audio;
-		this.range = range || 1;
-		this.divisionsInnerAngle = divisionsInnerAngle || 16;
-		this.divisionsOuterAngle = divisionsOuterAngle || 2;
-
-		var geometry = new BufferGeometry();
-		var divisions = this.divisionsInnerAngle + this.divisionsOuterAngle * 2;
-		var positions = new Float32Array( ( divisions * 3 + 3 ) * 3 );
-		geometry.addAttribute( 'position', new BufferAttribute( positions, 3 ) );
-
-		var materialInnerAngle = new LineBasicMaterial( { color: 0x00ff00 } );
-		var materialOuterAngle = new LineBasicMaterial( { color: 0xffff00 } );
-
-		Line.call( this, geometry, [ materialOuterAngle, materialInnerAngle ] );
-
-		this.update();
-
-	}
-
-	PositionalAudioHelper.prototype = Object.create( Line.prototype );
-	PositionalAudioHelper.prototype.constructor = PositionalAudioHelper;
-
-	PositionalAudioHelper.prototype.update = function () {
-
-		var audio = this.audio;
-		var range = this.range;
-		var divisionsInnerAngle = this.divisionsInnerAngle;
-		var divisionsOuterAngle = this.divisionsOuterAngle;
-
-		var coneInnerAngle = _Math.degToRad( audio.panner.coneInnerAngle );
-		var coneOuterAngle = _Math.degToRad( audio.panner.coneOuterAngle );
-
-		var halfConeInnerAngle = coneInnerAngle / 2;
-		var halfConeOuterAngle = coneOuterAngle / 2;
-
-		var start = 0;
-		var count = 0;
-		var i, stride;
-
-		var geometry = this.geometry;
-		var positionAttribute = geometry.attributes.position;
-
-		geometry.clearGroups();
-
-		//
-
-		function generateSegment( from, to, divisions, materialIndex ) {
-
-			var step = ( to - from ) / divisions;
-
-			positionAttribute.setXYZ( start, 0, 0, 0 );
-			count ++;
-
-			for ( i = from; i < to; i += step ) {
-
-				stride = start + count;
-
-				positionAttribute.setXYZ( stride, Math.sin( i ) * range, 0, Math.cos( i ) * range );
-				positionAttribute.setXYZ( stride + 1, Math.sin( Math.min( i + step, to ) ) * range, 0, Math.cos( Math.min( i + step, to ) ) * range );
-				positionAttribute.setXYZ( stride + 2, 0, 0, 0 );
-
-				count += 3;
-
-			}
-
-			geometry.addGroup( start, count, materialIndex );
-
-			start += count;
-			count = 0;
-
-		}
-
-		//
-
-		generateSegment( - halfConeOuterAngle, - halfConeInnerAngle, divisionsOuterAngle, 0 );
-		generateSegment( - halfConeInnerAngle, halfConeInnerAngle, divisionsInnerAngle, 1 );
-		generateSegment( halfConeInnerAngle, halfConeOuterAngle, divisionsOuterAngle, 0 );
-
-		//
-
-		positionAttribute.needsUpdate = true;
-
-		if ( coneInnerAngle === coneOuterAngle ) this.material[ 0 ].visible = false;
-
-	};
-
-	PositionalAudioHelper.prototype.dispose = function () {
-
-		this.geometry.dispose();
-		this.material[ 0 ].dispose();
-		this.material[ 1 ].dispose();
-
-	};
-
-	/**
 	 * @author mrdoob / http://mrdoob.com/
 	 * @author WestLangley / http://github.com/WestLangley
 	 */
@@ -47858,7 +47791,6 @@
 	exports.HemisphereLightHelper = HemisphereLightHelper;
 	exports.GridHelper = GridHelper;
 	exports.PolarGridHelper = PolarGridHelper;
-	exports.PositionalAudioHelper = PositionalAudioHelper;
 	exports.FaceNormalsHelper = FaceNormalsHelper;
 	exports.DirectionalLightHelper = DirectionalLightHelper;
 	exports.CameraHelper = CameraHelper;
