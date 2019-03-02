0e3bb80c3412b522c4c4d0f086e8838f50399f40
3jsLive <54175649+3jsLive@users.noreply.github.com>
2019-03-02 15:23:04 +0700

    415b3371a27ec9573885d7ce6f88a96d1730d3e8

--- build/three.js
+++ build/three.js
@@ -185,7 +185,7 @@
 
 	} );
 
-	var REVISION = '103dev';
+	var REVISION = '102';
 	var MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2 };
 	var CullFaceNone = 0;
 	var CullFaceBack = 1;
@@ -40190,7 +40190,8 @@
 
 			for ( var i = 0; i < 6; i ++ ) {
 
-				renderer.setRenderTarget( renderTarget, i );
+				renderTarget.activeCubeFace = i;
+				renderer.setRenderTarget( renderTarget );
 
 				renderer.clear( color, depth, stencil );
 
