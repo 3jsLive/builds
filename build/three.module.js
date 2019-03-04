818c83b8f9f517aa42184a18c1ff8dfd19648051
3jsLive <54175649+3jsLive@users.noreply.github.com>
2019-03-04 20:14:13 +0100

    3c5a5eb2e864c3aa1d5ef10519fad1850dee84be

--- build/three.module.js
+++ build/three.module.js
@@ -16345,7 +16345,7 @@ function getSingularSetter( type ) {
 		case 0x8b5c: return setValue4fm; // _MAT4
 
 		case 0x8b5e: case 0x8d66: return setValueT1; // SAMPLER_2D, SAMPLER_EXTERNAL_OES
-		case 0x8b5f: return setValueT3D1; // SAMPLER_3D
+		case 0x8B5F: return setValueT3D1; // SAMPLER_3D
 		case 0x8b60: return setValueT6; // SAMPLER_CUBE
 
 		case 0x1404: case 0x8b56: return setValue1i; // INT, BOOL
@@ -20102,7 +20102,7 @@ function WebGLTextures( _gl, extensions, state, properties, capabilities, utils,
 
 	//
 
-	var useOffscreenCanvas = typeof OffscreenCanvas !== 'undefined';
+	var useOffscreenCanvas = typeof document === 'undefined';
 
 	function createCanvas( width, height ) {
 
@@ -20149,6 +20149,16 @@ function WebGLTextures( _gl, extensions, state, properties, capabilities, utils,
 				canvas.height = height;
 
 				var context = canvas.getContext( '2d' );
+
+				// ImageBitmap is flipped vertically
+
+				if ( useOffscreenCanvas ) {
+
+					context.translate( 0, height );
+					context.scale( 1, - 1 );
+
+				}
+
 				context.drawImage( image, 0, 0, width, height );
 
 				console.warn( 'THREE.WebGLRenderer: Texture has been resized from (' + image.width + 'x' + image.height + ') to (' + width + 'x' + height + ').' );
@@ -24841,11 +24851,16 @@ function WebGLRenderer( parameters ) {
 
 	}() );
 
-	this.setTexture3D = function ( texture, slot ) {
+	this.setTexture3D = ( function () {
 
-		textures.setTexture3D( texture, slot );
+		// backwards compatibility: peel texture.texture
+		return function setTexture3D( texture, slot ) {
 
-	};
+			textures.setTexture3D( texture, slot );
+
+		};
+
+	}() );
 
 	this.setTexture = ( function () {
 
