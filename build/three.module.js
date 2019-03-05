2d3b613651c2df37f9c4a9776bdc84afac97f824
3jsLive <54175649+3jsLive@users.noreply.github.com>
2019-03-04 16:12:27 -0800

    a2e1493976812cee5cc64098e88edd64e5bf0acc

--- build/three.module.js
+++ build/three.module.js
@@ -16345,7 +16345,7 @@ function getSingularSetter( type ) {
 		case 0x8b5c: return setValue4fm; // _MAT4
 
 		case 0x8b5e: case 0x8d66: return setValueT1; // SAMPLER_2D, SAMPLER_EXTERNAL_OES
-		case 0x8B5F: return setValueT3D1; // SAMPLER_3D
+		case 0x8b5f: return setValueT3D1; // SAMPLER_3D
 		case 0x8b60: return setValueT6; // SAMPLER_CUBE
 
 		case 0x1404: case 0x8b56: return setValue1i; // INT, BOOL
@@ -20102,7 +20102,7 @@ function WebGLTextures( _gl, extensions, state, properties, capabilities, utils,
 
 	//
 
-	var useOffscreenCanvas = typeof document === 'undefined';
+	var useOffscreenCanvas = typeof OffscreenCanvas !== 'undefined';
 
 	function createCanvas( width, height ) {
 
@@ -20132,7 +20132,9 @@ function WebGLTextures( _gl, extensions, state, properties, capabilities, utils,
 
 			// only perform resize for certain image types
 
-			if ( image instanceof ImageBitmap || image instanceof HTMLImageElement || image instanceof HTMLCanvasElement ) {
+			if ( ( typeof HTMLImageElement !== 'undefined' && image instanceof HTMLImageElement ) ||
+				( typeof HTMLCanvasElement !== 'undefined' && image instanceof HTMLCanvasElement ) ||
+				( typeof ImageBitmap !== 'undefined' && image instanceof ImageBitmap ) ) {
 
 				var floor = needsPowerOfTwo ? _Math.floorPowerOfTwo : Math.floor;
 
@@ -20149,16 +20151,6 @@ function WebGLTextures( _gl, extensions, state, properties, capabilities, utils,
 				canvas.height = height;
 
 				var context = canvas.getContext( '2d' );
-
-				// ImageBitmap is flipped vertically
-
-				if ( useOffscreenCanvas ) {
-
-					context.translate( 0, height );
-					context.scale( 1, - 1 );
-
-				}
-
 				context.drawImage( image, 0, 0, width, height );
 
 				console.warn( 'THREE.WebGLRenderer: Texture has been resized from (' + image.width + 'x' + image.height + ') to (' + width + 'x' + height + ').' );
@@ -24851,16 +24843,11 @@ function WebGLRenderer( parameters ) {
 
 	}() );
 
-	this.setTexture3D = ( function () {
+	this.setTexture3D = function ( texture, slot ) {
 
-		// backwards compatibility: peel texture.texture
-		return function setTexture3D( texture, slot ) {
+		textures.setTexture3D( texture, slot );
 
-			textures.setTexture3D( texture, slot );
-
-		};
-
-	}() );
+	};
 
 	this.setTexture = ( function () {
 
