e8840443332f9774c71d95fed0fc0411b53fbaaa
3jsLive <54175649+3jsLive@users.noreply.github.com>
2019-05-09 17:46:05 -0400

    6703d9dc8b6c6464fb25ca82e78490e7d0a43fe0

--- build/three.module.js
+++ build/three.module.js
@@ -22702,7 +22702,7 @@ function WebGLRenderer( parameters ) {
 		 * Enables error checking and reporting when shader programs are being compiled
 		 * @type {boolean}
 		 */
-		checkShaderErrors: false
+		checkShaderErrors: true
 	};
 
 	// clearing
