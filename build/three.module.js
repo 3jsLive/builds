4715adc94f8b0e47f2395aa0f21ccd28dee9f851
3jsLive <54175649+3jsLive@users.noreply.github.com>
2019-05-28 15:42:37 -0700

    644a5f583ac3622063a528fdf0027b42c6037fcf

--- build/three.module.js
+++ build/three.module.js
@@ -22507,7 +22507,7 @@ function WebXRManager( renderer ) {
 			session.addEventListener( 'selectend', onSessionEvent );
 			session.addEventListener( 'end', onSessionEnd );
 
-			session.updateRenderState( {  baseLayer:  new XRWebGLLayer( session, gl ) } );
+			session.updateRenderState( { baseLayer: new XRWebGLLayer( session, gl ) } );
 
 			session.requestReferenceSpace( { type: 'stationary', subtype: 'eye-level' } ).then( onRequestFrameOfReference );
 
@@ -22594,8 +22594,6 @@ function WebXRManager( renderer ) {
 
 	function onAnimationFrame( time, frame ) {
 
-		let session = frame.session;
-
 		pose = frame.getViewerPose( frameOfReference );
 
 		if ( pose !== null ) {
@@ -22640,7 +22638,7 @@ function WebXRManager( renderer ) {
 
 				if ( inputPose !== null ) {
 
-					let targetRay = new XRRay(inputPose.transform);
+					var targetRay = new XRRay( inputPose.transform );
 					controller.matrix.elements = targetRay.matrix;
 
 					controller.matrix.decompose( controller.position, controller.rotation, controller.scale );
@@ -25054,7 +25052,7 @@ function WebGLRenderer( parameters ) {
 	//
 	this.setFramebuffer = function ( value ) {
 
-		if (_framebuffer !== value ) _gl.bindFramebuffer( 36160, value );
+		if ( _framebuffer !== value ) _gl.bindFramebuffer( 36160, value );
 
 		_framebuffer = value;
 
