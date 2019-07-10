015065991d8bc7bcfa92267f207303f490094afb
3jsLive <54175649+3jsLive@users.noreply.github.com>
2019-07-10 12:09:55 -0700

    5df3e9c292e5f4e199201e13ea0738e7835a0a53

--- build/three.js
+++ build/three.js
@@ -185,9 +185,8 @@
 
 	} );
 
-	var REVISION = '107dev';
-	var MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2, ROTATE: 0, DOLLY: 1, PAN: 2 };
-	var TOUCH = { ROTATE: 0, PAN: 1, DOLLY_PAN: 2, DOLLY_ROTATE: 3 };
+	var REVISION = '106';
+	var MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2 };
 	var CullFaceNone = 0;
 	var CullFaceBack = 1;
 	var CullFaceFront = 2;
@@ -49234,7 +49233,6 @@
 	exports.StringKeyframeTrack = StringKeyframeTrack;
 	exports.SubtractEquation = SubtractEquation;
 	exports.SubtractiveBlending = SubtractiveBlending;
-	exports.TOUCH = TOUCH;
 	exports.TangentSpaceNormalMap = TangentSpaceNormalMap;
 	exports.TetrahedronBufferGeometry = TetrahedronBufferGeometry;
 	exports.TetrahedronGeometry = TetrahedronGeometry;
