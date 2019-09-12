ea69a0b2951ac29d039fc2fc1a3cc08def96844d
3jsLive <54175649+3jsLive@users.noreply.github.com>
2019-09-12 10:44:43 -0400

    a859f9f2530e766b4a1f87a9987c7198417fafde

--- build/three.module.js
+++ build/three.module.js
@@ -18813,7 +18813,7 @@ function WebGLLights() {
 		pointShadowMatrix: [],
 		hemi: [],
 		hemiAffectedLayers: [],
-		
+
 		numDirectionalShadows: - 1,
 		numPointShadows: - 1,
 		numSpotShadows: - 1
