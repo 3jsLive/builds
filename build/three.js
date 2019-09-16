f064803c3243eb305a56e3550019ad2503b29211
3jsLive <54175649+3jsLive@users.noreply.github.com>
2019-09-16 10:55:39 +0100

    774b5c8383d1c9fb0e2d63fd62c1c2ef4da5ed21

--- build/three.js
+++ build/three.js
@@ -8358,8 +8358,8 @@
 		this.alphaTest = 0;
 		this.premultipliedAlpha = false;
 
-		this.parallelCompile = false;
 		this.visible = true;
+		this.parallelCompile = false;
 
 		this.toneMapped = true;
 
@@ -17951,7 +17951,7 @@
 		this.pending = false;
 		this.numMultiviewViews = numMultiviewViews;
 
-		if ( parallelShaderExt !== null && renderer.parallelCompile ) {
+		if ( parallelShaderExt !== null && material.parallelCompile ) {
 
 			if ( currentParallel < maxParallel ) {
 
@@ -18031,7 +18031,7 @@
 
 		compileAndLink: function ( renderer, material ) {
 
-			var gl = renderer.getContext();
+			var gl = renderer.context;
 
 			var program = this.program;
 			var vertexGlsl = this.prefixVertex + this.vertexShader;
@@ -18070,7 +18070,7 @@
 
 		isLinked: function ( renderer, material, sync ) {
 
-			var gl = renderer.getContext();
+			var gl = renderer.context;
 
 			if ( this.pending && currentParallel < maxParallel ) {
 
@@ -18098,7 +18098,7 @@
 
 			// check for link errors
 
-			var gl = renderer.getContext();
+			var gl = renderer.context;
 			var program = this.program;
 			var glVertexShader = this.vertexShader;
 			var glFragmentShader = this.fragmentShader;
--- build/three.module.js
+++ build/three.module.js
@@ -8352,8 +8352,8 @@ function Material() {
 	this.alphaTest = 0;
 	this.premultipliedAlpha = false;
 
-	this.parallelCompile = false;
 	this.visible = true;
+	this.parallelCompile = false;
 
 	this.toneMapped = true;
 
@@ -17940,7 +17940,7 @@ function WebGLProgram( renderer, extensions, code, material, shader, parameters,
 	this.pending = false;
 	this.numMultiviewViews = numMultiviewViews;
 
-	if ( parallelShaderExt !== null && renderer.parallelCompile ) {
+	if ( parallelShaderExt !== null && material.parallelCompile ) {
 
 		if ( currentParallel < maxParallel ) {
 
@@ -18020,7 +18020,7 @@ Object.assign( WebGLProgram.prototype, {
 
 	compileAndLink: function ( renderer, material ) {
 
-		var gl = renderer.getContext();
+		var gl = renderer.context;
 
 		var program = this.program;
 		var vertexGlsl = this.prefixVertex + this.vertexShader;
@@ -18059,7 +18059,7 @@ Object.assign( WebGLProgram.prototype, {
 
 	isLinked: function ( renderer, material, sync ) {
 
-		var gl = renderer.getContext();
+		var gl = renderer.context;
 
 		if ( this.pending && currentParallel < maxParallel ) {
 
@@ -18087,7 +18087,7 @@ Object.assign( WebGLProgram.prototype, {
 
 		// check for link errors
 
-		var gl = renderer.getContext();
+		var gl = renderer.context;
 		var program = this.program;
 		var glVertexShader = this.vertexShader;
 		var glFragmentShader = this.fragmentShader;
